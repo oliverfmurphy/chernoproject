7edf4093d9963059f2f7011e4e884af5d8f4ce50
Oliver Murphy <oliverfmurphy@gmail.com>
2014-04-30 07:42:06 +0100

    Handling Map Bounds

--- graphics/Sprite.java
+++ graphics/Sprite.java
@@ -7,7 +7,7 @@ public class Sprite {
 	public int[] pixels;
 	private SpriteSheet sheet;
 
-	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);//size, x, y,
+	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);//size, x, y, tile
 	public static Sprite voidSprite = new Sprite(16, 0x1B87E0); //www.colourpicker.com
 	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
