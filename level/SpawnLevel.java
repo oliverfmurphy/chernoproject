4b5374a15a56ba85705ac2163a457d24703c1c99
Oliver Murphy <oliverfmurphy@gmail.com>
2014-05-16 06:18:47 +0100

    55 - A bad way to map files

--- level/SpawnLevel.java
+++ level/SpawnLevel.java
@@ -2,9 +2,7 @@ package com.olivermurphy.chernoproject.level;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import com.olivermurphy.chernoproject.level.tile.Tile;
 
 public class SpawnLevel extends Level{
@@ -21,6 +19,7 @@ public class SpawnLevel extends Level{
         	int w = image.getWidth();
         	int h = image.getHeight();
         	tiles = new Tile[w * h];
+        	levelPixels = new int[w * h];
         	image.getRGB(0, 0, w, h, levelPixels, 0, w); // Converting image into an array of pixels
         } catch (IOException e){
         	e.printStackTrace();
@@ -30,14 +29,17 @@ public class SpawnLevel extends Level{
 	
 	// needs to convert an array of pixels into an array of tiles
 	// returns void and not Tile or Tile[] for instance to help performance and keep tile array intact
-	// Grass = 0xFF00
-	// Flower = 0xFFFF00
-	// Rock = 0x7F7F00
+	// Grass = 0xff00ff00
+	// Flower = 0xffffff00
+	// Rock = 0xff7f7f7f
 	protected void generateLevel() {
 		for (int i = 0; i < levelPixels.length; i++) {
-		    if (levelPixels[i] == 0xff00) tiles[i] = Tile.grass;
-		    if (levelPixels[i] == 0xffff00) tiles[i] = Tile.flower;
-		    if (levelPixels[i] == 0x7f7f00) tiles[i] = Tile.rock;
+			if (levelPixels[i] == 0xff00ff00) tiles[i] = Tile.grass; // preceding ff to fill up alpha channel, 00 opague, ff full opacity
+			else if (levelPixels[i] == 0xffffff00) tiles[i] = Tile.flower;
+			else if (levelPixels[i] == 0xff7f7f7f) {tiles[i] = Tile.rock;
+		    		System.out.println("Rock levelPixels[i]:" + levelPixels[i]);
+		    }
+		    else tiles[i] = Tile.voidTile;
 	    }
 	}
 
